086117f0e26751fdc436f7d0a3c078c96cdfa8de
nmhbim <hoangnm.nuce59@gmail.com>
2026-04-25 01:21:48 +0700

    chore: cleanup redundant JS files in src

--- src/lib/extractors/registry.js
+++
@@ -1,6 +0,0 @@
-import { YouTubeExtractor } from './youtube-extractor';
-import { JWPlayerExtractor } from './jwplayer-extractor';
-export const EXTRACTORS = [
-    new YouTubeExtractor(),
-    new JWPlayerExtractor()
-];
